966de06fdea585eccc6c745493741020dfb55b78
Lep Sistemas <lepsistemas@gmail.com>
2018-07-14 13:31:36 -0300

    Testing Eureka server

--- src/test/java/br/com/lepsistemas/brfinancialindex/discoveryservice/ServiceDiscoveryApplicationTests.java
+++ src/test/java/br/com/lepsistemas/brfinancialindex/discoveryservice/ServiceDiscoveryApplicationTests.java
@@ -1,16 +1,32 @@
 package br.com.lepsistemas.brfinancialindex.discoveryservice;
 
+import static org.hamcrest.Matchers.is;
+import static org.junit.Assert.assertThat;
+import static org.springframework.http.HttpStatus.OK;
+
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.web.client.TestRestTemplate;
+import org.springframework.boot.web.server.LocalServerPort;
+import org.springframework.http.ResponseEntity;
 import org.springframework.test.context.junit4.SpringRunner;
 
 @RunWith(SpringRunner.class)
-@SpringBootTest
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 public class ServiceDiscoveryApplicationTests {
+	
+	@LocalServerPort
+    private int port;
+	
+	@Autowired
+    private TestRestTemplate restTemplate;
 
 	@Test
-	public void contextLoads() {
+	public void should_start_eureka_server() {
+		ResponseEntity<String> entity = restTemplate.getForEntity("http://localhost:" + this.port + "/eureka/apps", String.class);
+		assertThat(entity.getStatusCode(), is(OK));
 	}
 
 }
